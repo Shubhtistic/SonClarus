376013c4a737a0b97dbf70a414abe1b977da6d7f
Shubham <theshubham1412@gmail.com>
2026-02-01 14:38:01 +0530

    Update: Added Annotated DbSessionDep in route

--- app/api_routes/job_status.py
+++ app/api_routes/job_status.py
@@ -1,15 +1,14 @@
 # This endpoint check the status for our job in the database
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, HTTPException, status
+from app.dependancies.db_dependancy import DbSessionDep
 from sqlalchemy.future import select
-from app.db.database_session import get_db_session
 from app.db.db_models import ProcessJob
 
 router = APIRouter()
 
 
 @router.get("/jobs/{job_id}")
-async def get_job_status(job_id: str, db: AsyncSession = Depends(get_db_session)):
+async def get_job_status(job_id: str, db: DbSessionDep):
     sql_query = select(ProcessJob).where(job_id == ProcessJob.job_id)
 
     result = await db.execute(sql_query)
