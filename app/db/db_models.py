a139db2f9cfb84cf6f1acdcd18dc0022e31bf649
Shubham <theshubham1412@gmail.com>
2026-02-03 17:38:10 +0530

    Update: Added full name in user table

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -20,6 +20,7 @@ class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     email: EmailStr = Field(unique=True, index=True)
     hashed_password: str
+    full_name: str = Field(default=None)
 
     storage_limit: int = Field(default=104_857_600)  # 100 MB = 104,857,600 bytes
     storage_used: int = Field(default=0)
