4661b155adfdecaf7a6667f4034a98b2897ca0f7
Shubham <theshubham1412@gmail.com>
2026-02-05 23:05:39 +0530

    Feat:Implement Login endpoint in auth.py

--- app/api_routes/auth.py
+++ app/api_routes/auth.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, status, HTTPException
+from datetime import timedelta
+from fastapi import APIRouter, status, HTTPException, Depends
 from app.schemas.user_schema import RegisterUser, UserRead
 
 from app.dependancies.db_dependancy import DbSessionDep  # async db session
 
 from app.db.db_models import User  # user table defn
 
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password, create_token
 from sqlalchemy.future import select
+from app.config import settings
+from app.schemas.token_schema import Token
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -37,3 +41,36 @@ async def register_user(user_data: RegisterUser, db: DbSessionDep):
     await db.commit()
     await db.refresh(new_user)
     return new_user
+
+
+@router.post("/login", response_model=Token)
+async def login(db: DbSessionDep, form_data: OAuth2PasswordRequestForm = Depends()):
+    """
+    takes form data -> username and password
+    verifies and then returns assigned jwt
+
+    """
+    # Note: form_data.username holds the email because of OAuth2 standards
+    query = select(User).where(User.email == form_data.username)
+    res = await db.execute(query)
+
+    final_res = res.scalar_one_or_none()
+
+    if not final_res or not verify_password(
+        form_data.password, final_res.hashed_password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User Or Password is wrong",
+        )
+
+    # Why we verifed Password and User at same time
+    ## Security: merge checks to prevent user enumeration attacks
+    # by returning the same error for both "sser Not found" and "wrong password",
+    # we hide valid emails from attackers, forcing them to guess blindly
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    # 'sub' (Subject) is the standard field for the user ID/Email in a token
+    token = create_token(data={"sub": final_res.email}, expires_in=access_token_expires)
+    return {"access_token": token, "token_type": "bearer"}
