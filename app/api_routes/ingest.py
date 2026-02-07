a07486564e90a01e10d4cb01692fc1bcb24c4678
Shubham <theshubham1412@gmail.com>
2026-02-07 17:09:05 +0530

    Update: Modified ingest endpoint, added user dependancy so only logged in users can can upload and used new Sql table 'job' instead of old deprecated 'processjob'

--- app/api_routes/ingest.py
+++ app/api_routes/ingest.py
@@ -1,7 +1,8 @@
 # this endpoints takes the files and checks if it an valid wav file
 
+import uuid
 from fastapi import APIRouter, UploadFile, File, HTTPException, status
-from app.db.db_models import ProcessJob
+from app.db.db_models import Job
 from app.dependancies.db_dependancy import DbSessionDep
 
 import shutil
@@ -11,7 +12,7 @@ import shutil
 import os
 # used to talk with the computer/our device create files or folders
 
-from uuid import uuid4
+# from uuid import uuid4
 
 # uuid4 -> unique id with veruy low chance of replication
 
@@ -19,6 +20,8 @@ from app.schemas.file_upload import FileUpload
 from app.config import settings
 
 from app.celery_app import process_audio_file
+
+from app.dependancies.auth import CurrentUserDep
 
 
 UPLOAD_DIR = settings.FILE_UPLOAD_PATH
@@ -33,34 +36,36 @@ router = APIRouter()
 
 @router.post("/uploads", response_model=FileUpload)
 async def upload_audio(
-    db: DbSessionDep, file: UploadFile = File(...)
+    current_user: CurrentUserDep, db: DbSessionDep, file: UploadFile = File(...)
 ):  ### ... means this endpoint cant be hit without an file
     if not file.filename.endswith(".wav"):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="The provided File is Invalid.\nFile should be an audio file with '.wav' format",
         )
-    job_id = str(uuid4())[:16]
 
-    new_filename = f"{job_id}_{file.filename}"
+    file_extension = file.filename.split(".")[-1]
+    new_filename = f"{uuid.uuid4()}.{file_extension}"
 
     file_path = os.path.join(UPLOAD_DIR, new_filename)
     # os.path.join(): Safely combines the folder name and the filename.
+    try:
+        with open(file_path, "wb") as audio_file:
+            shutil.copyfileobj(file.file, audio_file)
+            # we copy the incoming file into our folder
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Could not save file: {str(e)}")
 
-    with open(file_path, "wb") as audio_file:
-        shutil.copyfileobj(file.file, audio_file)
-        # we copy the incoming file into our folder
-
-    new_job = ProcessJob(job_id=job_id, filename=new_filename)
+    new_job = Job(filename=file.filename, filepath=file_path, user_id=current_user.id)
     db.add(new_job)
     await db.commit()
     await db.refresh(new_job)
 
-    process_audio_file.delay(job_id, file_path)
+    task = process_audio_file.delay(str(new_job.id))
 
-    return FileUpload(
-        job_id=job_id,
-        status="PENDING",
-        filename=new_filename,
-        message="File ingested successfully.",
-    )
+    return {
+        "message": "Upload successful",
+        "job_id": new_job.id,
+        "status": "pending",
+        "task_id": task.id,
+    }
