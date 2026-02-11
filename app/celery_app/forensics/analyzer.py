2cc6e52c0dca3e03f77815294f14683bda5ad9ad
Shubham <theshubham1412@gmail.com>
2026-02-11 14:32:15 +0530

    Fix: Fixed typos in analyzer.py

--- app/celery_app/forensics/analyzer.py
+++ app/celery_app/forensics/analyzer.py
@@ -6,7 +6,10 @@ import numpy as np
 
 def generate_waveform_image(file_path: str, output_path: str):
     # why we did sr=None as librosa shrinks sample rate of file
-    # we dont wan to shrik sample rate wo de sr=None
+    # we dont want to shrik sample rate so we do sr=None
+
+
+    #Also mono = false .. avoid mono conversion
     y, sr = librosa.load(file_path, sr=None, mono=False)
 
     # Create a wide canvas (10x4 inches)
@@ -26,7 +29,7 @@ def generate_waveform_image(file_path: str, output_path: str):
 
     # tight_layout() -> remove white borders white borders.
     # savefig() -> write file to disk.
-    # close() -> this is imp matpltlib keep the graph in memory so we need to close or server will crash after multiple uploads
+    # close() -> this is imp as matpltlib keeps the graph in memory so we need to close it or server will crash after multiple uploads
 
 
 def generate_spectrogram_image(file_path: str, output_path: str):
@@ -38,7 +41,7 @@ def generate_spectrogram_image(file_path: str, output_path: str):
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
 
     # The Math (Short-Time Fourier Transform)
-    # reason ->  e need to un-mix the sound to see the frequencies
+    # reason ->  we need to un-mix the sound to see the frequencies
 
     # stft(y): Breaks sound into frequencies
     # abs(): Gets the strength of the frequency
