2692fb33b656544045458bfc4ea2302f8c008894
Shubham <theshubham1412@gmail.com>
2026-05-05 01:14:22 +0530

    Refactor(db): removed transcript field from job table as we store the transcript in s3, no need to save it we only save summary to db, so it can easily be displayed to the user

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,11 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 from sqlmodel import SQLModel, Field, DateTime, Relationship, Column, String
 from sqlalchemy.dialects.postgresql import JSONB
 from datetime import datetime, timezone
 from uuid import UUID
 from uuid_utils import uuid7
 from pydantic import EmailStr
-
 from enum import Enum
 
 
@@ -24,40 +23,33 @@ class User(SQLModel, table=True):
     email: EmailStr = Field(unique=True, index=True)
     hashed_password: str
     full_name: Optional[str] = Field(default=None)
-
     is_active: bool = Field(default=True)
-
-    storage_limit: int = Field(default=104857600)  # 100 MB = 104,857,600 bytes
+    storage_limit: int = Field(default=104857600)  # 100 MB
     storage_used: int = Field(default=0)
-
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         sa_type=DateTime(timezone=True),
     )
-    # Relationship: A User has a list of Jobs
-    jobs: list["Job"] = Relationship(back_populates="user")
-
-    refresh_tokens: list["RefreshToken"] = Relationship(back_populates="user")
+    jobs: List["Job"] = Relationship(back_populates="user")
+    refresh_tokens: List["RefreshToken"] = Relationship(back_populates="user")
 
 
 class Job(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid7, primary_key=True)
-
     filename: str
     object_key: str
 
     status: JobStatus = Field(default=JobStatus.QUEUED)
 
-    # The Foreign Key (Link to the Parent)
-    user_id: UUID = Field(foreign_key="user.id")
+    is_denoise: bool = Field(default=True)
+    is_separation: bool = Field(default=True)
+    is_transcription: bool = Field(default=True)
 
-    # Relationship: A Job belongs to one User
+    user_id: UUID = Field(foreign_key="user.id")
     user: Optional[User] = Relationship(back_populates="jobs")
 
-    transcript: list[dict] | None = Field(default=None, sa_column=Column(JSONB))
-    summary: str | None = Field(default=None, sa_column=Column(String))
-    action_items: list[str] | None = Field(default=None, sa_column=Column(JSONB))
-    error_message: str | None = Field(default=None, sa_column=Column(String))
+    summary: Optional[str] = Field(default=None, sa_column=Column(String))
+    error_message: Optional[str] = Field(default=None, sa_column=Column(String))
 
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
