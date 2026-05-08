df1b3fb78065e35bbb4579206cc7b57fb4d63758
Shubham <theshubham1412@gmail.com>
2026-05-08 12:09:36 +0530

    Refactor: changed jobs endpoints queries to only return completed jobs

--- app/api_routes/job.py
+++ app/api_routes/job.py
@@ -22,20 +22,25 @@ async def get_user_jobs(
     """Fetch jobs for the user, including the summary"""
     user_id = user.id
 
-    # base query for user
-    base_qry = select(Job).where(Job.user_id == user_id)
-
     # total count
-    count_qry = select(func.count()).select_from(base_qry.subquery())
+    count_qry = (
+        select(func.count())
+        .where(Job.user_id == user_id)
+        .where(Job.status == JobStatus.DONE)
+    )
     total_count = (await db.execute(count_qry)).scalar()
 
     # data to be returned to frontend
-    data_qry = select(
-        Job.id,
-        Job.filename,
-        Job.summary,
-        Job.created_at,
-    ).where(Job.user_id == user_id)
+    data_qry = (
+        select(
+            Job.id,
+            Job.filename,
+            Job.summary,
+            Job.created_at,
+        )
+        .where(Job.user_id == user_id)
+        .where(Job.status == JobStatus.DONE)
+    )
 
     # Apply sorting
     if sort == "desc":
